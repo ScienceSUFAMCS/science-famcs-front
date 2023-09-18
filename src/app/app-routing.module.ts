d087104a145afca6a5e0b4279b53c293bf57cf4d
KrollikRoddzer <krollikroddzer@mail.ru>
2023-09-18 14:31:23 +0300

    feat: added shared module, header component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from './shared/shared.module';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule, SharedModule]
 })
 export class AppRoutingModule { }
