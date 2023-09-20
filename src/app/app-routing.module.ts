b02614905db3e313ce2b6d1ae27ba40391afbddc
KrollikRoddzer <krollikroddzer@mail.ru>
2023-09-20 16:34:25 +0300

    fix: removed exporting Shared module from app-routing.module and imported it in app.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,6 @@ const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule, SharedModule]
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
