cc6f5ee006e5c1d9321a4f9c71de684d05b0664e
KrollikRoddzer <krollikroddzer@mail.ru>
2023-09-25 11:34:53 +0300

    feat: added night theme, created theme-service

---
+++ src/app/shared/header/service/theme.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeService {
+  private theme : string = 'tuiIconSunLarge';
+  public readonly standartThemeIndex : number = 0;
+  public readonly themes : string[] = ['tuiIconSunLarge', 'tuiIconMoonLarge'];
+
+  constructor() { 
+    if (localStorage.getItem('app-theme') !== null) {
+      this.theme = String(localStorage.getItem('app-theme'));
+    }
+    else {
+      this.theme = this.themes[this.standartThemeIndex];
+      localStorage.setItem('app-theme', this.theme);
+    }
+  }
+
+  public getTheme() : string {
+    return this.theme;
+  }
+
+  public changeTheme() : void {
+    if (this.theme === this.themes[0]) {
+      this.theme = this.themes[1];
+    }
+    else {
+      this.theme = this.themes[0];
+    }
+    localStorage.setItem('app-theme', this.theme);
+  }
+
+  public isDark() : boolean {
+    return this.themes[1] === this.theme;
+  }
+}
