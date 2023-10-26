2373054662ac2a928732623a34e759260732a561
KrollikRoddzer <krollikroddzer@mail.ru>
2023-10-26 18:05:43 +0300

    feat: refactored register form and added form contriols to the form

--- src/app/account/components/register/register.component.ts
+++ src/app/account/components/register/register.component.ts
@@ -1,6 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import {TuiCountryIsoCode} from '@taiga-ui/i18n';
 
 @Component({
   selector: 'app-register',
@@ -10,12 +9,12 @@ import {TuiCountryIsoCode} from '@taiga-ui/i18n';
 })
 
 export class RegisterComponent {
-  readonly registerFormGroup = new FormGroup({
-    sexInput: new FormControl(),
-    phone: new FormControl(),
+  readonly registerForm = new FormGroup({
+    firstName : new FormControl(''),
+    lastName : new FormControl(''),
+    patronymic : new FormControl(''),
+    login : new FormControl(''),
+    password : new FormControl(''),
+    repeatPassword: new FormControl(''),
   });
-
-  readonly contries = Object.values(TuiCountryIsoCode);
-  
-  countruIsoCode = TuiCountryIsoCode.BY;
 }
