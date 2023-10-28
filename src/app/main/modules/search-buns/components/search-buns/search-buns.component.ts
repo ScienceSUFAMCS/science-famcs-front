98ae20fea1f10401c993a9f29fe3d76166fce659
Aleksanya <allexandra.ya@gmail.com>
2023-10-28 20:05:50 +0300

    feat: add search form with filter

--- src/app/main/modules/search-buns/components/search-buns/search-buns.component.ts
+++ src/app/main/modules/search-buns/components/search-buns/search-buns.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-search-buns',
   templateUrl: './search-buns.component.html',
-  styleUrls: ['./search-buns.component.scss']
+  styleUrls: ['./search-buns.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SearchBunsComponent {
+  search = ''
+
+  filterForm = new FormGroup({
+    filters: new FormControl(['Конспекты', 'Методички', 'Книги', 'Другое'])
+  });
+
+  readonly items = [
+    'Конспекты',
+    'Методички',
+    'Книги',
+    'Другое'
+];
 
 }
