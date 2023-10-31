914235a7adf2477e718b15dfe76bf571664b7793
Aleksanya <allexandra.ya@gmail.com>
2023-11-01 00:29:21 +0300

    feat: add inputs in search, search service, interface queryFile, fileType; fix: FormControl, button

--- src/app/main/modules/search-buns/components/search-buns/search-buns.component.ts
+++ src/app/main/modules/search-buns/components/search-buns/search-buns.component.ts
@@ -1,14 +1,20 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { SearchDataService } from './service/search-data.service';
+import { QueryFile } from 'src/app/shared/interface/query-file.interface';
+
 
 @Component({
   selector: 'app-search-buns',
   templateUrl: './search-buns.component.html',
   styleUrls: ['./search-buns.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [SearchDataService]
 })
-export class SearchBunsComponent {
-  search = ''
+export class SearchBunsComponent implements OnInit {
+  subject = ''
+  name = ''
+  author = ''
 
   readonly items = [
     'Конспекты',
@@ -17,9 +23,21 @@ export class SearchBunsComponent {
     'Другое'
   ];
 
-  filterForm = new FormGroup({
-    filters: new FormControl(this.items)
-  });
+  SearchForm: FormGroup = new FormGroup({
+    subject: new FormControl(),
+    name: new FormControl(),
+    author: new FormControl(),
+    filter: new FormGroup({
+      filters: new FormControl(this.items)
+    })
+  })
+
+  searchData: QueryFile[] =[];
+  constructor(private searchDataService: SearchDataService) { }
+
+  ngOnInit() {
+    this.searchData = this.searchDataService.getData();
+  }
 
   readonly subjects = [
     { name: 'ДМиМЛ', description: 'Дискретная математика и математическая логика' },
@@ -38,5 +56,9 @@ export class SearchBunsComponent {
     'var(--tui-support-16)',
     'var(--tui-support-14)'
   ]
+
+  onSubmit(): void {
+    alert('поиск не работает :(')
+  }
 
 }
---
+++ src/app/main/modules/search-buns/components/search-buns/service/search-data.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { QueryFile } from 'src/app/shared/interface/query-file.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchDataService {
+  private data: QueryFile[] = [
+    { name: 'Математический анализ, 1 семестр', author: 'Мазаник А.С', subject: 'ДиИИ', type: 'конспекты' },
+    { name: 'Курс лекций по математическому анализу для студентов 1 курса ФПМИ специальности “Информатика“, 1 сем', author: 'Булатов В.И', subject: 'ДИиИ', type: 'конспекты' },
+    { name: 'Лекция 1. Булевы функции, 2 семестр', author: 'Ловеров', subject: 'ДМиМЛ', type: 'конспекты' },
+    { name: 'Конспект ДМиМЛ рукописный', author: 'Орлович Ю.Л.', subject: 'ДМиМЛ', type: 'конспекты' },
+  ];
+  getData(): QueryFile[] {
+    return this.data;
+  }
+}
